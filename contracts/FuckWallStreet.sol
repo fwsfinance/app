e9ff0569fe64d72b56c32aa27cdb03954b75ab21
fwsfinance <info@fws.finance>
2021-01-31 18:07:30 +0100

    updated contract

--- contracts/FuckWallStreet.sol
+++ contracts/FuckWallStreet.sol
@@ -5,9 +5,17 @@ import "@openzeppelin/contracts/token/ERC20/ERC20Capped.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract FuckWallStreet is Ownable, ERC20, ERC20Capped {
-  event ClaimRequestEvent(string redditUser, address ethAddress);
+  event ClaimRequestEvent(bytes32 requestId);
   event ClaimConfirmEvent(string redditUser, address ethAddress, uint256 amount);
-  mapping(string => uint) public claims;
+
+  struct Claim {
+    string redditUser;
+    address ethAddress;
+    uint256 amount;
+    bool confirmed;
+  }
+  mapping(bytes32 => Claim) public claims;
+  mapping(string => uint256) public claimedAmountsByUser;
 
   constructor()
     public
@@ -15,17 +23,27 @@ contract FuckWallStreet is Ownable, ERC20, ERC20Capped {
     ERC20Capped(2750000000 * (10**uint256(18)))
   {}
 
-  function _beforeTokenTransfer(address from, address to, uint256 amount) internal override(ERC20, ERC20Capped) {
-    super._beforeTokenTransfer(from, to, amount);
+  function _beforeTokenTransfer(address _from, address _to, uint256 _amount) internal override(ERC20, ERC20Capped) {
+    super._beforeTokenTransfer(_from, _to, _amount);
   }
 
-  function claimRequest(string calldata redditUser) public payable {
+  function claimRequest(string calldata _redditUser) public payable {
     require(msg.value > 0, "Oracle call must be payed for.");
-    emit ClaimRequestEvent(redditUser, msg.sender);
+    payable(owner()).transfer(msg.value);
+    bytes32 requestId = bytes32(keccak256(abi.encodePacked(_redditUser, block.timestamp)));
+    claims[requestId] = Claim(_redditUser, msg.sender, 0, false);
+    emit ClaimRequestEvent(requestId);
   }
 
-  function claimConfirm(string calldata _redditUser, address _ethAddress, uint256 _amount) public onlyOwner {
-    _mint(_ethAddress, _amount);
-    ClaimConfirmEvent(_redditUser, _ethAddress, _amount);
+  function claimConfirm(bytes32 _requestId, uint256 _amount) public onlyOwner {
+    require(claims[_requestId].confirmed == false, "Claim already confirmed.");
+    require(claimedAmountsByUser[claims[_requestId].redditUser] == 0, "You already claimed your airdrop.");
+    claims[_requestId].amount = _amount;
+
+    _mint(claims[_requestId].ethAddress, claims[_requestId].amount);
+
+    claimedAmountsByUser[claims[_requestId].redditUser] += claims[_requestId].amount;
+    claims[_requestId].confirmed = true;
+    ClaimConfirmEvent(claims[_requestId].redditUser, claims[_requestId].ethAddress, claims[_requestId].amount);
   }
 }
