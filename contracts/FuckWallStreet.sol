3d9498c9610a1cb4959909b78171541d3b32217b
fwsfinance <info@fws.finance>
2021-02-01 23:13:41 +0100

    put claim tiers into contract and implemented moderator/owner rewards

--- contracts/FuckWallStreet.sol
+++ contracts/FuckWallStreet.sol
@@ -3,25 +3,32 @@ pragma solidity ^0.6.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20Capped.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
 
 contract FuckWallStreet is Ownable, ERC20, ERC20Capped {
   event ClaimRequestEvent(bytes32 requestId);
-  event ClaimConfirmEvent(string redditUser, address ethAddress, uint256 amount);
+  event ClaimConfirmEvent(string redditUser, address ethAddress, uint8 tier);
 
   struct Claim {
     string redditUser;
     address ethAddress;
-    uint256 amount;
+    uint8 tier;
     bool confirmed;
   }
   mapping(bytes32 => Claim) public claims;
-  mapping(string => uint256) public claimedAmountsByUser;
+  mapping(string => bool) public hasClaimed;
+  address[] private mods;
+  uint256[] private tierAmounts;
 
   constructor()
     public
     ERC20("FuckWallStreet", "FWS")
-    ERC20Capped(2750000000 * (10**uint256(18)))
-  {}
+    ERC20Capped(2750000000 * 10**18)
+  {
+    tierAmounts.push(500 * 10**18);
+    tierAmounts.push(750 * 10**18);
+    tierAmounts.push(1500 * 10**18);
+  }
 
   function _beforeTokenTransfer(address _from, address _to, uint256 _amount) internal override(ERC20, ERC20Capped) {
     super._beforeTokenTransfer(_from, _to, _amount);
@@ -35,15 +42,33 @@ contract FuckWallStreet is Ownable, ERC20, ERC20Capped {
     emit ClaimRequestEvent(requestId);
   }
 
-  function claimConfirm(bytes32 _requestId, uint256 _amount) public onlyOwner {
+  function claimConfirm(bytes32 _requestId, uint8 _tier, bool _isMod) public onlyOwner {
     require(claims[_requestId].confirmed == false, "Claim already confirmed.");
-    require(claimedAmountsByUser[claims[_requestId].redditUser] == 0, "You already claimed your airdrop.");
-    claims[_requestId].amount = _amount;
+    require(hasClaimed[claims[_requestId].redditUser] == false, "You already claimed your airdrop.");
 
-    _mint(claims[_requestId].ethAddress, claims[_requestId].amount);
-
-    claimedAmountsByUser[claims[_requestId].redditUser] += claims[_requestId].amount;
+    claims[_requestId].tier = _tier;
     claims[_requestId].confirmed = true;
-    ClaimConfirmEvent(claims[_requestId].redditUser, claims[_requestId].ethAddress, claims[_requestId].amount);
+    hasClaimed[claims[_requestId].redditUser] = true;
+
+    _mint(claims[_requestId].ethAddress, tierAmounts[_tier]);
+
+    ClaimConfirmEvent(claims[_requestId].redditUser, claims[_requestId].ethAddress, claims[_requestId].tier);
+
+    if (_isMod == true) {
+      mods.push(msg.sender);
+    } else {
+      uint256 fivePercent = SafeMath.div(tierAmounts[_tier], 20);
+      uint256 tenPercent = SafeMath.div(tierAmounts[_tier], 10);
+
+      if (mods.length > 0) {
+        uint256 perMod = SafeMath.div(fivePercent, mods.length);
+        for (uint i = 0; i < mods.length; i++) {
+          _mint(mods[i], perMod);
+        }
+        _mint(owner(), fivePercent);
+      } else {
+        _mint(owner(), tenPercent);
+      }
+    }
   }
 }
